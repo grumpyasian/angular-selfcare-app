056520bd94682e9058018641eb5becf4f6d085ab
grumpyasian <gabbie.gq@gmail.com>
2023-06-15 16:58:12 +0200

    Added components and commented out everything expect mat-tab.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { RoutineComponent } from './routine/routine.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/menu', pathMatch: 'full' },
+  { path: 'menu', component: MenuComponent },
+  { path: 'routine', component: RoutineComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
