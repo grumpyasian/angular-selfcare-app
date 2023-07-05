362eba524dc1ca5c105ff8f8456385bdf31c831b
grumpyasian <gabbie.gq@gmail.com>
2023-07-05 15:35:53 +0200

    Fixed menu button and menu tab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { TodoComponent } from './todo/todo.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
+  { path: '', component: MenuComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'routine', component: RoutineComponent },
   { path: 'calendar', component: CalendarComponent },
