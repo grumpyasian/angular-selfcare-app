50a72a7b88346f991dbc8370e8aba359bf1a530d
grumpyasian <gabbie.gq@gmail.com>
2023-07-26 19:41:38 +0200

    can create todos in the routine-card

--- src/app/routine/routine-card/routine-card.component.ts
+++ src/app/routine/routine-card/routine-card.component.ts
@@ -1,12 +1,26 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-routine-card',
   templateUrl: './routine-card.component.html',
   styleUrls: ['./routine-card.component.scss']
 })
-
 export class RoutineCardComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  newTodoText: string = ''; // This will hold the text of the new todo
+  showAddTodoInput: boolean = false; // Whether to show the input field for adding a new todo
+
+  constructor(
+    public dialogRef: MatDialogRef<RoutineCardComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  // Method to add the new todo to the card's content
+  addTodo() {
+    // Here you would need to update this logic based on your card structure
+    const newTodo = { icon: 'todo_icon', title: this.newTodoText };
+    this.data.content.push(newTodo);
+    this.newTodoText = ''; // Reset the input field after adding the todo
+    this.showAddTodoInput = false; // Hide the input field after adding the todo
+  }
 }
--- src/app/routine/routine.component.ts
+++ src/app/routine/routine.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, EventEmitter } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { RoutineCardComponent } from './routine-card/routine-card.component';
 import { CardService } from '../card.service';
@@ -9,83 +9,41 @@ import { Card, DateType } from '../card.model';
   templateUrl: './routine.component.html',
   styleUrls: ['./routine.component.scss']
 })
-
 export class RoutineComponent implements OnInit {
-  cardsChangedEvent: EventEmitter<void>;
-  cards: Card[] = []; // Update the type to Card[]
+  cards: Card[] = [];
 
-  constructor(private dialog: MatDialog, private elementRef: ElementRef, private cardService: CardService) {
-    this.cardsChangedEvent = this.cardService.onCardsChanged();
-  }
+  constructor(private dialog: MatDialog, private elementRef: ElementRef, private cardService: CardService) {}
 
   ngOnInit() {
-    this.cardsChangedEvent.subscribe(() => {
-      this.getCards(); // Update the view or fetch latest cards
-    });
-
-    // Initial retrieval of cards
-    this.getCards();
-  }
-
-  getCards() {
     this.cards = this.cardService.getCards();
-    // Example: Log the retrieved cards to the console
-    console.log('Retrieved Cards:', this.cards);
-    // You can also perform other actions based on the retrieved cards
   }
 
-  addCard() {
-    const newCardData = 'New Card'; // Example: New card data
-    const dateType = DateType.Custom; // Example: Set the dateType (you can customize this based on your requirements)
-    const time = new Date().toLocaleTimeString(); // Example: Get the current time as a string
+  handleClick(card: Card, event: MouseEvent) {
+    // Check if the click target is the mat-icon with class "more-icon"
+    const targetElement = event.target as HTMLElement;
+    const isMoreIconClicked = targetElement.classList.contains('more-icon');
 
-    this.cardService.addCard(newCardData, dateType, time); // Add the new card with data, dateType, and time
+    // If the click target is not the mat-icon with class "more-icon", open the dialog
+    if (!isMoreIconClicked) {
+      this.openRoutineCardDialog(card);
+    }
   }
-  
+
+  openRoutineCardDialog(card: Card) {
+    this.dialog.open(RoutineCardComponent, {
+      data: {
+        title: card.title,
+        time: card.time,
+        content: [
+          // Customize this content based on your requirements
+          // { icon: 'bed', title: 'Make Bed', content: 'Make your bed every morning.' },
+          // Add more content items as needed
+        ],
+      },
+    });
+  }
 
   deleteCard(card: Card): void {
-    this.cardService.deleteCard(card.id); // Delete the specified card by ID
-  }
-  
-
-  // deleteCard(card: string): void {
-  //   this.cardService.deleteCard(card);
-  // }
-  
-  
-  handleClick(event: MouseEvent, dialogRoutineCardId: string) {
-    const targetElement = event.target as HTMLElement;
-    const preventDialogOpening = targetElement.classList.contains('mat-icon');
-
-    if (!preventDialogOpening) {
-      this.openDialogRoutineCardById(dialogRoutineCardId);
-    }
-  }
-
-  openDialogRoutineCardById(dialogRoutineCardId: string) {
-    const dialogContent = this.getDialogRoutineCardContent(dialogRoutineCardId);
-    if (dialogContent) {
-      this.dialog.open(RoutineCardComponent, {
-        data: dialogContent
-      });
-    }
-  }
-
-  private getDialogRoutineCardContent(dialogRoutineCardId: string) {
-    switch (dialogRoutineCardId) {
-      case 'dialogRoutine1':
-        return {
-          title: 'morning routine',
-          time: '9:00-9:30 (30m)'
-        };
-      case 'dialogRoutine2':
-        return {
-          title: 'night routine',
-          time: '21:00-21:30 (30m)'
-        };
-      default:
-        return null;
-    }
+    this.cardService.deleteCard(card.id);
   }
 }
-
